1992e041958e19802862863c49f9fb2455ac1527
Rishabh Ranjan <rishabh9050@gmail.com>
2023-09-13 17:48:42 +0530

    Added Templates

--- app.py
+++ app.py
@@ -1,38 +1,25 @@
-# -------------------------------------------
-# from flask import Flask
+# Import Flask module from flask package
+from flask import Flask 
 
-# app = Flask(__name__)
+# Used to render template files onto flask route
+from flask import render_template 
 
-# @app.route("/")
+# Create an app using name as '__name__'
+app = Flask(__name__) 
 
-# def hello_world():
-#     return "<p>Hello World!</p>"
-
-# app.run(debug=True)
-
-# -------------------------------------------
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-# -------------------------------------------
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
+# Refers to the url 127.0.0.1:1999/
+@app.route('/') 
 def hello_world():
-    return 'Hello, World!'
+    # Returns content of index.html
+    return render_template('index.html')
+    # return 'Hello, World!'
 
+# Refers to the url 127.0.0.1:1999/home/
 @app.route('/home')
 def home_print():
     return "<h1>This is home route</h1>"
 
+# If the name of the app is '__main__',
+# It will run the app at port=1999
 if __name__ == '__main__':
     app.run(debug=True, port=1999)
